4aa026391a21d03c01c3881be4741eb8d89ba24d
amcquistan <adam.mcquistan@thecodinginterface.com>
2021-08-16 18:33:37 +0000

    update to run Cloud Language ML

--- immersion/docker_and_kubernetes/adams-playground/quote-sentiment/app.py
+++ immersion/docker_and_kubernetes/adams-playground/quote-sentiment/app.py
@@ -4,6 +4,7 @@ import requests
 
 from bs4 import BeautifulSoup
 from flask import Flask, jsonify
+from google.cloud import language_v1
 from pydantic import BaseModel
 
 
@@ -14,8 +15,27 @@ class Quote(BaseModel):
     text : str
     author : str
     tags : typing.Sequence[str]
-
-
+    sentiment : typing.Optional[float]
+    magnitude : typing.Optional[float]
+    
+    def calc_sentiment(self):
+        client = language_v1.LanguageServiceClient()
+        doc = {
+          'content': self.text,
+          'type_': language_v1.Document.Type.PLAIN_TEXT,
+          'language': 'en' 
+        }
+        
+        request = {
+            'document': doc,
+            'encoding_type': language_v1.EncodingType.UTF8
+        }
+        response = client.analyze_sentiment(request)
+        
+        self.sentiment = response.document_sentiment.score
+        self.magnitude = response.document_sentiment.magnitude
+        
+        
 @app.route('/')
 def index():
     quote_url = 'https://quotes.toscrape.com/random'
@@ -32,5 +52,7 @@ def index():
         tags=[el.get_text() for el in quote_el.find_all('a', class_='tag')]
     )
 
+    quote.calc_sentiment()
+    
     return jsonify(quote.dict())
 
